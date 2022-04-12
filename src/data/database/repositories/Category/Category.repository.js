f982b8062b101730ec9c11b17a2e04da5dd4fad9
igorcruzz <igorskt2009@gmail.com>
2022-04-12 15:00:30 -0300

    refactor: add function to find by id and update

--- src/data/database/repositories/Category/Category.repository.js
+++ src/data/database/repositories/Category/Category.repository.js
@@ -16,9 +16,30 @@ export class CategoryRepository {
     });
   }
 
+  async findCategoryById({ id }) {
+    return await Categoria.findOne({
+      where: {
+        ID_CATEGORIA: id,
+      },
+      raw: true,
+    });
+  }
+
   async deleteCategory({ id }) {
     await Categoria.destroy({
       where: { ID_CATEGORIA: id },
+    });
+  }
+
+  async updateCategory({ id, name }) {
+    const category = await Categoria.findOne({
+      where: {
+        ID_CATEGORIA: id,
+      },
+    });
+
+    return category.update({
+      NM_CATEGORIA: name.toLowerCase().trim(),
     });
   }
 }
