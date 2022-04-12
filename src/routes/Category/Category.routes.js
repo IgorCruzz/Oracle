37318a8018ad132d10e13757d3111963e2fb0058
igorcruzz <igorskt2009@gmail.com>
2022-04-12 15:52:35 -0300

    refactor: add comments

--- src/routes/Category/Category.routes.js
+++ src/routes/Category/Category.routes.js
@@ -3,31 +3,43 @@ import {
   DeleteCategoryController,
   CreateCategoryController,
   UpdateCategoryController,
+  FindCategoriesController,
 } from '../../data/controllers';
 import {
   createCategoryValidator,
   deleteCategoryValidator,
   updateCategoryValidator,
+  findCategoriesValidator,
 } from '../../data/validators';
 
 const routes = Router();
 
+// Body name
 routes.post(
   '/categories',
   createCategoryValidator,
   new CreateCategoryController().handle
 );
 
+// Param :id && Body name
 routes.delete(
-  '/categories/:name',
+  '/categories/:id',
   deleteCategoryValidator,
   new DeleteCategoryController().handle
 );
 
+// Param :id && Body name
 routes.put(
   '/categories/:id',
   updateCategoryValidator,
   new UpdateCategoryController().handle
+);
+
+// Query ?limit &&  ?page
+routes.get(
+  '/categories',
+  findCategoriesValidator,
+  new FindCategoriesController().handle
 );
 
 export default routes;
