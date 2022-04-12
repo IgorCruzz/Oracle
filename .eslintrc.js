8441435ca6d34efe6332c34651c63b9006149899
igorcruzz <igorskt2009@gmail.com>
2022-04-12 11:29:44 -0300

    feat: add new rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
     "no-param-reassign": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern":"next"}],
-    "import/prefer-default-export": "off"
+    "import/prefer-default-export": "off",
+    "import/export": "off"
 
   },
 };
