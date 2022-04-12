58fe30511af8b9aba36d7461a679086164d203de
igorcruzz <igorskt2009@gmail.com>
2022-04-12 15:48:34 -0300

    refactor: export controller

--- src/data/controllers/index.js
+++ src/data/controllers/index.js
@@ -1,3 +1,4 @@
 export * from './Category/CreateCategory.controller';
 export * from './Category/DeleteCategory.controller';
 export * from './Category/UpdateCategory.controller';
+export * from './Category/FindCategories.controller';
