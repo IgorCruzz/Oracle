da52eca93cfd14f7f15895e67e88f36a4fd879f4
igorcruzz <igorskt2009@gmail.com>
2022-04-12 12:11:31 -0300

    feat: createCategoryValidator

---
+++ src/data/validators/Category/createCategory.validator.js
@@ -0,0 +1,18 @@
+import * as Yup from 'yup';
+
+export const createCategoryValidator = async (req, res, next) => {
+  try {
+    const Schema = Yup.object().shape({
+      name: Yup.string().required(),
+    });
+
+    await Schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (e) {
+    return res.status(400).json({
+      error: 'Erro na validação',
+      messages: e.errors,
+    });
+  }
+};
