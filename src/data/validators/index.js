1674294031ded6282efc3df28ab736b16dbe5978
igorcruzz <igorskt2009@gmail.com>
2022-04-12 15:51:23 -0300

    refactor: add findcategories validator

--- src/data/validators/index.js
+++ src/data/validators/index.js
@@ -1,3 +1,4 @@
 export * from './Category/createCategory.validator';
 export * from './Category/deleteCategory.validator';
 export * from './Category/updateCategory.validator';
+export * from './Category/findCategories.validator';
