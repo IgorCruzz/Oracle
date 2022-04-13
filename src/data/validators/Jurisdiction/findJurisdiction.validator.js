c8853051385de77199956a88c89d6ba2ef20ed6d
igorcruzz <igorskt2009@gmail.com>
2022-04-13 16:52:20 -0300

    feat: add find Jurisdiction validator

---
+++ src/data/validators/Jurisdiction/findJurisdiction.validator.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export const findJurisdictionsValidator = async (req, res, next) => {
+  try {
+    const SchemaParam = Yup.object().shape({
+      limit: Yup.string().required(),
+      page: Yup.string().required(),
+    });
+
+    await SchemaParam.validate(req.query, { abortEarly: false });
+
+    return next();
+  } catch (e) {
+    return res.status(400).json({
+      error: 'Validation error',
+      messages: e.errors,
+    });
+  }
+};
