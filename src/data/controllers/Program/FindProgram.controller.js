f6812c97fa2e7abc7c29fd7aa16f7872ba15b628
igorcruzz <igorskt2009@gmail.com>
2022-04-13 11:00:18 -0300

    feat: add findProgramController

---
+++ src/data/controllers/Program/FindProgram.controller.js
@@ -0,0 +1,26 @@
+import { FindProgramService } from '../../services';
+
+export class FindProgramController {
+  async handle(req, res) {
+    try {
+      const { page, limit } = req.query;
+
+      const service = new FindProgramService();
+
+      const response = await service.execute({ page, limit });
+
+      if (response.error)
+        return res.status(400).json({
+          error: response.error,
+        });
+
+      return res.status(200).json({
+        programs: response.programs,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        body: err,
+      });
+    }
+  }
+}
