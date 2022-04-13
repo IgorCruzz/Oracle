ace39a553b444f67242ce3d929cfe325e032151d
igorcruzz <igorskt2009@gmail.com>
2022-04-13 13:11:29 -0300

    feat: add  updateregion service

---
+++ src/data/controllers/Region/UpdateRegion.controller.js
@@ -0,0 +1,27 @@
+import { UpdateRegionService } from '../../services';
+
+export class UpdateRegionController {
+  async handle(req, res) {
+    try {
+      const service = new UpdateRegionService();
+
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const response = await service.execute({ name, id });
+
+      if (response.error)
+        return res.status(400).json({
+          error: response.error,
+        });
+
+      return res.status(200).json({
+        region: response.region,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        body: err,
+      });
+    }
+  }
+}
