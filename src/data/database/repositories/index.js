4dbf61698dc23cc830d1c1cd3b9d4779c1ce5374
igorcruzz <igorskt2009@gmail.com>
2022-04-13 17:46:22 -0300

    feat: add Agency repository

--- src/data/database/repositories/index.js
+++ src/data/database/repositories/index.js
@@ -3,3 +3,4 @@ export * from './Program/Program.repository';
 export * from './Region/Region.repository';
 export * from './City/City.repository';
 export * from './Jurisdiction/Jurisdiction.repository';
+export * from './Agency/Agency.repository';
