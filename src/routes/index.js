e7c701de7ae84ea81a5ac019a90ca57de36068f4
igorcruzz <igorskt2009@gmail.com>
2022-04-12 23:36:00 -0300

    feat: add route to test connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,6 @@ const routes = [Category];
 
 const router = Router();
 
+router.get('/ping', (req, res) => res.status(200).json({ data: 'PONG' }));
+
 export const exposeRoutes = routes.map(routerMap => router.use('/', routerMap));
