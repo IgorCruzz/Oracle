8baae934d4952f57807aba9c70af4c3925599178
igorcruzz <igorskt2009@gmail.com>
2022-04-13 15:49:20 -0300

    feat: add findCity controller

---
+++ src/data/controllers/City/FindCity.controller.js
@@ -0,0 +1,26 @@
+import { FindCitiesService } from '../../services';
+
+export class FindCityController {
+  async handle(req, res) {
+    try {
+      const { page, limit, regionId } = req.query;
+
+      const service = new FindCitiesService();
+
+      const response = await service.execute({ page, limit, regionId });
+
+      if (response.error)
+        return res.status(400).json({
+          error: response.error,
+        });
+
+      return res.status(200).json({
+        cities: response.cities,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        body: err,
+      });
+    }
+  }
+}
