978d2c4e53e897d5f3aa762a7ac85119e156e052
igorcruzz <igorskt2009@gmail.com>
2022-04-13 17:48:55 -0300

    feat: create agency validator

---
+++ src/data/validators/Agency/createAgency.validator.js
@@ -0,0 +1,19 @@
+import * as Yup from 'yup';
+
+export const createAgencyValidator = async (req, res, next) => {
+  try {
+    const Schema = Yup.object().shape({
+      name: Yup.string().required(),
+      jurisdictionId: Yup.number().required(),
+    });
+
+    await Schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (e) {
+    return res.status(400).json({
+      error: 'Validation error',
+      messages: e.errors,
+    });
+  }
+};
