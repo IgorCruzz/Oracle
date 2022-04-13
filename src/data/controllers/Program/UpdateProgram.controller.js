e92e25c74cf46cbc73ab0790b5dc98bf152ec20b
igorcruzz <igorskt2009@gmail.com>
2022-04-13 11:00:29 -0300

    feat: add updateProgramController

---
+++ src/data/controllers/Program/UpdateProgram.controller.js
@@ -0,0 +1,27 @@
+import { UpdateProgramService } from '../../services';
+
+export class UpdateProgramController {
+  async handle(req, res) {
+    try {
+      const service = new UpdateProgramService();
+
+      const { id } = req.params;
+      const { name } = req.body;
+
+      const response = await service.execute({ name, id });
+
+      if (response.error)
+        return res.status(400).json({
+          error: response.error,
+        });
+
+      return res.status(200).json({
+        program: response.program,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        body: err,
+      });
+    }
+  }
+}
