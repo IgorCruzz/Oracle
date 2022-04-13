a220dbf05b58ed55c53c2119bf0d1eea890eeb1d
igorcruzz <igorskt2009@gmail.com>
2022-04-13 16:52:06 -0300

    feat: add create Jurisdiction validator

---
+++ src/data/validators/Jurisdiction/createJurisdiction.validator.js
@@ -0,0 +1,18 @@
+import * as Yup from 'yup';
+
+export const createJurisdictionValidator = async (req, res, next) => {
+  try {
+    const Schema = Yup.object().shape({
+      name: Yup.string().required(),
+    });
+
+    await Schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (e) {
+    return res.status(400).json({
+      error: 'Validation error',
+      messages: e.errors,
+    });
+  }
+};
