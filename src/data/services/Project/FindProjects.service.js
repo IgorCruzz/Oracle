5c1c4feb5ccc63f405006a2054f4efbd0fe55d08
igorcruzz <igorskt2009@gmail.com>
2022-04-20 17:17:24 -0300

    feat: add find project controller

---
+++ src/data/services/Project/FindProjects.service.js
@@ -0,0 +1,21 @@
+import { AgencyRepository } from '../../database/repositories';
+
+export class FindProjectsService {
+  async execute({ page, limit, jurisdictionId, search }) {
+    const repository = new AgencyRepository();
+
+    const findAgencies = await repository.findAgencies({
+      limit,
+      page,
+      jurisdictionId,
+      search,
+    });
+
+    if (findAgencies.length === 0)
+      return { error: 'Não há nenhum orgão registrado.' };
+
+    return {
+      agencies: findAgencies,
+    };
+  }
+}
