3182ef67fd396ce939e6192f07fedea93b3debac
igorcruzz <igorskt2009@gmail.com>
2022-04-20 17:14:25 -0300

    feat: add create project controller

---
+++ src/data/controllers/Project/CreateProject.controller.js
@@ -0,0 +1,25 @@
+import { CreateProjectService } from '../../services';
+
+export class CreateProjectController {
+  async handle(req, res) {
+    try {
+      const service = new CreateProjectService();
+
+      const response = await service.execute(req.body);
+
+      if (response.error)
+        return res.status(400).json({
+          error: response.error,
+        });
+
+      return res.status(200).json({
+        message: response.message,
+        project: response.project,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        error: 'Internal Server Error',
+      });
+    }
+  }
+}
