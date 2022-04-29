2bd22e8a09de8ea68fb8523d658ee7019277843f
igorcruzz <igorskt2009@gmail.com>
2022-04-29 16:06:43 -0300

    refactor: add attr

--- src/data/database/repositories/User/User.repository.js
+++ src/data/database/repositories/User/User.repository.js
@@ -2,20 +2,27 @@ import { User } from '../../models';
 
 export class UserRepository {
   async createUser(data) {
-    const { ds_email_login, nm_user, password } = data;
+    const { ds_email_login, nm_user } = data;
 
     const createdLocation = await User.create({
       ds_email_login: ds_email_login.trim(),
       nm_user: nm_user && nm_user.trim(),
-      ds_password: password,
+      ...data,
       dt_created_at: new Date(Date.now()).toISOString(),
       dt_updated_at: new Date(Date.now()).toISOString(),
     });
 
     return await User.findOne({
       where: {
-        ds_address: createdLocation.dataValues.ds_address,
+        ds_email_login: createdLocation.dataValues.ds_email_login,
       },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
     });
   }
 
@@ -23,6 +30,13 @@ export class UserRepository {
     return await User.findAndCountAll({
       limit: Number(limit),
       offset: (Number(page) - 1) * Number(limit),
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
     });
   }
 
@@ -31,6 +45,13 @@ export class UserRepository {
       where: {
         ds_email_login,
       },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
       raw: true,
     });
   }
@@ -40,23 +61,29 @@ export class UserRepository {
       where: {
         nm_user,
       },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
       raw: true,
     });
   }
 
-  async findUserById({ id_user, populate }) {
-    if (populate) {
-      return await User.findOne({
-        where: {
-          id_user,
-        },
-      });
-    }
-
+  async findUserById({ id_user }) {
     return await User.findOne({
       where: {
         id_user,
       },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
       raw: true,
     });
   }
@@ -64,6 +91,13 @@ export class UserRepository {
   async deleteUser({ id_user }) {
     await User.destroy({
       where: { id_user },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
     });
   }
 
@@ -83,6 +117,13 @@ export class UserRepository {
       where: {
         id_user: user.dataValues.id_user,
       },
+      attributes: [
+        'id_user',
+        'ds_email_login',
+        'nm_user',
+        'dt_created_at',
+        'dt_updated_at',
+      ],
     });
   }
 }
