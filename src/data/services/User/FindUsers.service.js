e122b81d7cae7bcbc4ef48c4b02bd858f18023db
igorcruzz <igorskt2009@gmail.com>
2022-04-29 16:06:22 -0300

    refactor: add query

--- src/data/services/User/FindUsers.service.js
+++ src/data/services/User/FindUsers.service.js
@@ -1,10 +1,10 @@
 import { UserRepository } from '../../database/repositories';
 
 export class FindUsersService {
-  async execute() {
+  async execute({ page, limit }) {
     const repository = new UserRepository();
 
-    const findUsers = await repository.findUsers();
+    const findUsers = await repository.findUsers({ page, limit });
 
     if (findUsers.length === 0)
       return { error: 'Não há nenhum usuário registrado.' };
