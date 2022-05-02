ac30591f83c442d19dd501c1c72b868720eb05c5
igorcruzz <igorskt2009@gmail.com>
2022-05-02 13:51:57 -0300

    refactor: add user object on sign jwt

--- src/data/services/Login/Login.service.js
+++ src/data/services/Login/Login.service.js
@@ -24,7 +24,11 @@ export class LoginService {
     return {
       message: 'Login realizado com sucesso!',
       access_token: jwt.sign(
-        { id: verifyEmailExists.id_user },
+        {
+          user: {
+            id: verifyEmailExists.id_user,
+          },
+        },
         'f04af61b3f332afa0ceec786a42cd365',
         {
           expiresIn: '72h',
