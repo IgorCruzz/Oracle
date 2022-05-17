8a17241d647cee4701ce8c5a815d78dfff34afb2
igorcruzz <igorskt2009@gmail.com>
2022-05-17 13:12:27 -0300

    refactor: add conditional to verify fk

--- src/data/services/Professional/CreateProfessional.service.js
+++ src/data/services/Professional/CreateProfessional.service.js
@@ -1,9 +1,46 @@
-import { ProfessionalRepository } from '../../database/repositories';
+import {
+  ProfessionalRepository,
+  RoleGradeRepository,
+  UserRepository,
+  SectorRepository,
+} from '../../database/repositories';
 
 export class CreateProfessionalService {
   async execute(data) {
-    const { nm_professional } = data;
+    const { nm_professional, id_sector, id_role_grade, id_user } = data;
     const repository = new ProfessionalRepository();
+    const sectorRepository = new SectorRepository();
+    const roleGradeRepoistory = new RoleGradeRepository();
+    const userRepository = new UserRepository();
+
+    const verifySectorId = await sectorRepository.findSectorById({
+      id_sector,
+    });
+
+    if (!verifySectorId)
+      return {
+        error: `Não há nenhuma Setor registrado com este ID -> ${id_sector}.`,
+      };
+
+    const verifyRoleGradeId = await roleGradeRepoistory.findRoleGradeById({
+      id_role_grade,
+    });
+
+    if (!verifyRoleGradeId)
+      return {
+        error: `Não há nenhum Custo HH registrado com este ID -> ${id_role_grade}.`,
+      };
+
+    if (id_user) {
+      const verifyUserId = await userRepository.findUserById({
+        id_user,
+      });
+
+      if (!verifyUserId)
+        return {
+          error: `Não há nenhum usuário registrado com este ID -> ${id_user}.`,
+        };
+    }
 
     const verifyProfessionalExists = await repository.findProfessional({
       nm_professional,
