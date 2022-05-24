61d589397406d31cdefb4709ce843edf11f0aa09
igorcruzz <igorskt2009@gmail.com>
2022-05-23 23:21:35 -0300

    refactor: fix validations

--- src/data/validators/AllocationPeriod/createAllocationPeriod.validator.js
+++ src/data/validators/AllocationPeriod/createAllocationPeriod.validator.js
@@ -4,18 +4,25 @@ import { ValidationError } from '../../../utils/validationError';
 export const createAllocationPeriodValidator = async (req, res, next) => {
   try {
     const Schema = Yup.object().shape({
-      dt_start_allocation: Yup.string().required(
-        'A Data inicial do período de Alocação é obrigatória'
-      ),
-      dt_end_allocation: Yup.string().required(
-        'A Data final do período de Alocação é obrigatória'
-      ),
+      dt_start_allocation: Yup.string()
+        .required('A Data inicial do período de Alocação é obrigatória')
+        .typeError(
+          'O preenchimento da Data inicial do período de Alocação é obrigatório'
+        ),
+      dt_end_allocation: Yup.string()
+        .required('A Data final do período de Alocação é obrigatória')
+        .typeError(
+          'O preenchimento da Data final do período de Alocação é obrigatório'
+        ),
       qt_business_hours: Yup.number()
         .positive(
           'A quantidade de horas úteis do período de alocação precisa ser positiva'
         )
         .required(
           'O campo quantidade de horas úteis do período de alocação é obrigatória'
+        )
+        .typeError(
+          'O preenchimento da quantidade de horas úteis do período de alocação é obrigatório'
         ),
     });
 
--- src/data/validators/AllocationPeriod/updateAllocationPeriod.validator.js
+++ src/data/validators/AllocationPeriod/updateAllocationPeriod.validator.js
@@ -8,17 +8,26 @@ export const updateAllocationPeriodValidator = async (req, res, next) => {
     });
 
     const SchemaBody = Yup.object().shape({
-      dt_start_allocation: Yup.string().required(
-        'O campo Data inicial de Alocação é obrigatório'
-      ),
-      dt_end_allocation: Yup.string().required(
-        'O campo Data final de Alocação é obrigatório'
-      ),
+      dt_start_allocation: Yup.string()
+        .required('A Data inicial do período de Alocação é obrigatória')
+        .typeError(
+          'O preenchimento da Data inicial do período de Alocação é obrigatório'
+        ),
+      dt_end_allocation: Yup.string()
+        .required('A Data final do período de Alocação é obrigatória')
+        .typeError(
+          'O preenchimento da Data final do período de Alocação é obrigatório'
+        ),
       qt_business_hours: Yup.number()
         .positive(
-          'O campo quantidade de horas de trabalho precisa ser positivo'
+          'A quantidade de horas úteis do período de alocação precisa ser positiva'
         )
-        .required('O campo quantidade de horas de trabalho é obrigatório'),
+        .required(
+          'O campo quantidade de horas úteis do período de alocação é obrigatória'
+        )
+        .typeError(
+          'O preenchimento da quantidade de horas úteis do período de alocação é obrigatório'
+        ),
     });
 
     await SchemaParam.validate(req.params, { abortEarly: false });
