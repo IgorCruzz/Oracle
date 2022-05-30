e0e4160e75db9b6521a01f8a29d9361259ed2c3b
igorcruzz <igorskt2009@gmail.com>
2022-05-30 15:31:29 -0300

    feat: add conditional

--- src/data/services/Document/UploadDocument.service.js
+++ src/data/services/Document/UploadDocument.service.js
@@ -4,6 +4,16 @@ export class UploadDocumentService {
   async execute(id_document, { filename }) {
     const repository = new DocumentRepository();
 
+    const findDocument = await repository.findDocumentById({
+      id_document,
+    });
+
+    if (!findDocument) {
+      return {
+        error: `Não há um Documento com este ID -> ${id_document}`,
+      };
+    }
+
     await repository.updateDocument(id_document, {
       dt_upload: new Date(Date.now()).toISOString(),
       nm_file: filename,
