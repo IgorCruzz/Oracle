2e9ab63994c655f74bf203aa090fced3a1f7df07
igorcruzz <igorskt2009@gmail.com>
2022-06-06 14:36:49 -0300

    refactor: fix calculate

--- src/utils/calculateHour.js
+++ src/utils/calculateHour.js
@@ -1,6 +1,6 @@
 export const calculateHour = ({ value, max, min, prov }) => {
   // const totalDuration = (max * 1 + min * 1 + prov * 1 * 4) / 6;
-  const totalDuration = (min * 1 + max * 1 + prov * 4) / 6;
+  const totalDuration = ((min * 1 + max * 1 + prov * 4) / 6) * 1.15;
 
   const result = value === 1 ? totalDuration : totalDuration * 0.3;
 
