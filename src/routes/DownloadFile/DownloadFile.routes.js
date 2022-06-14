27457c41bace3b66b976ab9af9ae814e6f6deb56
igorcruzz <igorskt2009@gmail.com>
2022-06-14 17:56:51 -0300

    refactor: remove auth

--- src/routes/DownloadFile/DownloadFile.routes.js
+++ src/routes/DownloadFile/DownloadFile.routes.js
@@ -4,7 +4,7 @@ import authenticator from '../../data/authenticator/jwt.authenticator';
 
 const routes = Router();
 
-routes.get('/download/:filename', authenticator, (req, res) => {
+routes.get('/download/:filename', (req, res) => {
   const { filename } = req.params;
   const file = `${__dirname}/tmp/documents/${filename}`;
   return res.download(file);
