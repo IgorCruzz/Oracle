bf7857f57bbb7cd47c11cea2c45f7fea4fba50d8
igorcruzz <igorskt2009@gmail.com>
2022-07-08 14:44:16 -0300

    feat: create powerbi project

--- src/data/services/PowerBi/PowerBiProject.service.js
+++ src/data/services/PowerBi/PowerBiProject.service.js
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import {
   Project,
   Project_phase,
@@ -6,6 +7,9 @@ import {
   Product,
   Location,
   Product_history,
+  User,
+  Professional,
+  Allocation_period,
 } from '../../database/models';
 import { calculateHour } from '../../../utils/calculateHour';
 
@@ -57,6 +61,26 @@ export class PowerBiProjectService {
                 {
                   model: Product_history,
                   as: 'product_history',
+                  include: [
+                    {
+                      model: Allocation_period,
+                      as: 'allocation',
+                    },
+                    {
+                      model: Professional,
+                      as: 'professional',
+                    },
+                    {
+                      model: User,
+                      as: 'user',
+                      include: [
+                        {
+                          model: Professional,
+                          as: 'professional',
+                        },
+                      ],
+                    },
+                  ],
                 },
                 {
                   model: Project_phase,
@@ -93,56 +117,144 @@ export class PowerBiProjectService {
     await Promise.all(
       projects.map(async project => {
         project.dataValues.project_phase.map(result => {
-          const product_result = result.dataValues.product.map(product => {
-            return Data.push({
-              nm_project:
-                product.dataValues.project_phase.dataValues.project.dataValues
-                  .nm_project,
-              nm_city:
-                product.dataValues.project_phase.dataValues.project.dataValues
-                  .city.dataValues.nm_city,
-              cd_priority:
-                product.dataValues.project_phase.dataValues.project.cd_priority,
-              cd_complexity:
-                product.dataValues.project_phase.dataValues.project
-                  .cd_complexity,
-              nm_region:
-                product.dataValues.project_phase.dataValues.project.dataValues
-                  .city.dataValues.region.dataValues.nm_region,
-              cd_sei:
-                product.dataValues.project_phase.dataValues.project.dataValues
-                  .cd_sei || '',
-              tx_description:
-                product.dataValues.project_phase.dataValues.project.dataValues
-                  .tx_description || '',
-              phase_type_code:
-                product.dataValues.project_phase.tp_project_phase || '',
-              phase_type_name: product.dataValues.project_phase.tp_project_phase
-                ? (product.dataValues.project_phase.tp_project_phase === 10 &&
-                    'Concepção') ||
-                  (product.dataValues.project_phase.tp_project_phase === 20 &&
-                    'Priorização') ||
-                  (product.dataValues.project_phase.tp_project_phase === 30 &&
-                    'Desenvolvimento') ||
-                  (product.dataValues.project_phase.tp_project_phase === 40 &&
-                    'Licitação') ||
-                  (product.dataValues.project_phase.tp_project_phase === 50 &&
-                    'Execução') ||
-                  (product.dataValues.project_phase.tp_project_phase === 60 &&
-                    'Encerrado em Garantia')
-                : '',
-              nm_project_phase:
-                product.dataValues.project_phase.nm_project_phase,
-              nm_product: product.dataValues.nm_product,
-              tp_required_action: product.dataValues.tp_required_action,
-              hours: calculateHour({
-                max: product.dataValues.qt_maximum_hours,
-                min: product.dataValues.qt_minimum_hours,
-                prov: product.dataValues.qt_probable_hours,
-                value: product.dataValues.tp_required_action,
-              }),
-            });
-          });
+          const product_result = result.dataValues.product.map(
+            async product => {
+              return Data.push({
+                nm_project:
+                  product.dataValues.project_phase.dataValues.project.dataValues
+                    .nm_project,
+                nm_city:
+                  product.dataValues.project_phase.dataValues.project.dataValues
+                    .city.dataValues.nm_city,
+                cd_priority:
+                  product.dataValues.project_phase.dataValues.project
+                    .cd_priority,
+                cd_complexity:
+                  product.dataValues.project_phase.dataValues.project
+                    .cd_complexity,
+                nm_region:
+                  product.dataValues.project_phase.dataValues.project.dataValues
+                    .city.dataValues.region.dataValues.nm_region,
+                cd_sei:
+                  product.dataValues.project_phase.dataValues.project.dataValues
+                    .cd_sei || '',
+                tx_description:
+                  product.dataValues.project_phase.dataValues.project.dataValues
+                    .tx_description || '',
+                phase_type_code:
+                  product.dataValues.project_phase.tp_project_phase || '',
+                phase_type_name: product.dataValues.project_phase
+                  .tp_project_phase
+                  ? (product.dataValues.project_phase.tp_project_phase === 10 &&
+                      'Concepção') ||
+                    (product.dataValues.project_phase.tp_project_phase === 20 &&
+                      'Priorização') ||
+                    (product.dataValues.project_phase.tp_project_phase === 30 &&
+                      'Desenvolvimento') ||
+                    (product.dataValues.project_phase.tp_project_phase === 40 &&
+                      'Licitação') ||
+                    (product.dataValues.project_phase.tp_project_phase === 50 &&
+                      'Execução') ||
+                    (product.dataValues.project_phase.tp_project_phase === 60 &&
+                      'Encerrado em Garantia')
+                  : '',
+                nm_project_phase:
+                  product.dataValues.project_phase.nm_project_phase,
+                nm_product: product.dataValues.nm_product,
+                tp_required_action:
+                  (product.dataValues.tp_required_action === 0 &&
+                    'Não Definida') ||
+                  (product.dataValues.tp_required_action === 1 &&
+                    'Produção Integral') ||
+                  (product.dataValues.tp_required_action === 2 &&
+                    'Produção Parcial') ||
+                  (product.dataValues.tp_required_action === 3 &&
+                    'Dispensado') ||
+                  (product.dataValues.tp_required_action === 4 &&
+                    'Concluído pelo demandante'),
+                hours: calculateHour({
+                  max: product.dataValues.qt_maximum_hours,
+                  min: product.dataValues.qt_minimum_hours,
+                  prov: product.dataValues.qt_probable_hours,
+                  value: product.dataValues.tp_required_action,
+                }),
+                pti: `${format(
+                  new Date(
+                    product.dataValues.product_history[
+                      product.dataValues.product_history.length - 1
+                    ].allocation.dt_start_allocation
+                  ),
+                  'dd/MM/yyyy'
+                )}-${format(
+                  new Date(
+                    product.dataValues.product_history[
+                      product.dataValues.product_history.length - 1
+                    ].allocation.dt_end_allocation
+                  ),
+                  'dd/MM/yyyy'
+                )}(${
+                  product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].allocation.qt_business_hours
+                }h)`,
+                nm_professional:
+                  product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].professional.nm_professional,
+                cd_status:
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 0 &&
+                    'Ag. Alocação') ||
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 1 &&
+                    'Em Produção') ||
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 2 &&
+                    'Em Análise') ||
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 3 &&
+                    'Em Correção') ||
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 4 &&
+                    'Em Análise de Correção') ||
+                  (product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 5 &&
+                    'Concluído'),
+                dt_concluded:
+                  product.dataValues.product_history[
+                    product.dataValues.product_history.length - 1
+                  ].cd_status === 5
+                    ? format(
+                        new Date(
+                          product.dataValues.product_history[
+                            product.dataValues.product_history.length - 1
+                          ].dt_created_at
+                        ),
+                        'dd/MM/yyyy'
+                      )
+                    : '',
+                nm_analyst_user: product.dataValues.product_history[
+                  product.dataValues.product_history.length - 1
+                ].user
+                  ? product.dataValues.product_history[
+                      product.dataValues.product_history.length - 1
+                    ].user.dataValues.professional
+                    ? product.dataValues.product_history[
+                        product.dataValues.product_history.length - 1
+                      ].user.dataValues.professional.nm_professional
+                    : product.dataValues.product_history[
+                        product.dataValues.product_history.length - 1
+                      ].user.nm_user
+                  : '',
+              });
+            }
+          );
 
           if (product_result.length === 0) {
             Data.push({
@@ -169,6 +281,11 @@ export class PowerBiProjectService {
               nm_product: '',
               tp_required_action: '',
               hours: '',
+              pti: '',
+              nm_professional: '',
+              cd_status: '',
+              dt_concluded: '',
+              nm_analyst_user: '',
             });
           }
         });
@@ -188,6 +305,11 @@ export class PowerBiProjectService {
             nm_product: '',
             tp_required_action: '',
             hours: '',
+            pti: '',
+            nm_professional: '',
+            cd_status: '',
+            dt_concluded: '',
+            nm_analyst_user: '',
           });
         }
       })
