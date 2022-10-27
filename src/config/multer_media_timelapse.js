6e5dafc86e33c86240cccae69bd3d41dfb4f7c04
igorcruzz <igorskt2009@gmail.com>
2022-10-27 20:30:43 -0300

    refactor: comment feature

--- src/config/multer_media_timelapse.js
+++ src/config/multer_media_timelapse.js
@@ -1,31 +1,31 @@
-import aws from 'aws-sdk';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import crypto from 'crypto';
-import { extname } from 'path';
+// import aws from 'aws-sdk';
+// import multer from 'multer';
+// import multerS3 from 'multer-s3';
+// import crypto from 'crypto';
+// import { extname } from 'path';
 
-const spacesEndpoint = new aws.Endpoint('sfo3.digitaloceanspaces.com');
+// const spacesEndpoint = new aws.Endpoint('sfo3.digitaloceanspaces.com');
 
-aws.config.update({
-  accessKeyId: 'DO0098U9A8D6HJZNNT6R',
-  secretAccessKey: '83GJZKHnCH57T3obii3FW6qFcGTKS2a3FgumIM7GcZs',
-});
+// aws.config.update({
+//   accessKeyId: 'DO0098U9A8D6HJZNNT6R',
+//   secretAccessKey: '83GJZKHnCH57T3obii3FW6qFcGTKS2a3FgumIM7GcZs',
+// });
 
-const s3 = new aws.S3({
-  endpoint: spacesEndpoint,
-});
+// const s3 = new aws.S3({
+//   endpoint: spacesEndpoint,
+// });
 
-export const storage = multer({
-  storage: multerS3({
-    s3,
-    bucket: 'gerobras-development/media_timelapses',
-    acl: 'public-read',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key(request, file, cb) {
-      cb(
-        null,
-        `${crypto.randomBytes(10).toString('Hex')}${extname(file.originalname)}`
-      );
-    },
-  }),
-});
+// export const storage = multer({
+//   storage: multerS3({
+//     s3,
+//     bucket: 'gerobras-development/media_timelapses',
+//     acl: 'public-read',
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     key(request, file, cb) {
+//       cb(
+//         null,
+//         `${crypto.randomBytes(10).toString('Hex')}${extname(file.originalname)}`
+//       );
+//     },
+//   }),
+// });
